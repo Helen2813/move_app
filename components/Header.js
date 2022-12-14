5c7f7214fc4fb8ba496c53e90c13ef2cb597c38c
Elena Krikun <elena.krikun@quantori.com>
2022-12-14 12:50:56 +0400

    created Header component

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import Image from "next/image";
+import HeaderIcon from "./HeaderIcon";
+import { HomeIcon, UsersIcon, PhoneIcon, InformationCircleIcon } from "@heroicons/react/20/solid";
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <div className="">
+        <HeaderIcon Icon={HomeIcon} title="HOME" />
+        <HeaderIcon Icon={UsersIcon} title="ACCOUNT" />
+        <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
+        <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
+      </div>
+      <Image
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/IMDB_-_SuperTinyIcons.svg/1200px-IMDB_-_SuperTinyIcons.svg.png"
+        width="100"
+        height="100"
+        alt="logo"
+      />
+    </div>
   );
 }
 
