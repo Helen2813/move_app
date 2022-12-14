2fe3c46fb2bcb4000ff69e47dbdaba354e6736e0
Elena Krikun <elena.krikun@quantori.com>
2022-12-14 18:23:46 +0400

    created the navbar components

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import requests from "../utils/requests";
+import { useRouter } from "next/router";
 
 function Navbar() {
+  const router = useRouter();
+
   return (
-    <div>Navbar</div>
+    <div className="flex justify-center bg-gray-600 text-gray-200 select-none text-xl lg:text-2xl">
+      {Object.entries(requests).map(( [key, { title, url }]) => (
+        <p
+          className="m-6 cursor-pointer hover:text-white active:text-red-400"
+          key={key}
+          onClick={() => (router.push(`?genre=${key}`))}
+        >
+          {title}
+        </p>
+      ))}
+    </div>
   );
 }
 
